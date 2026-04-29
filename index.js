87517f36bdf3af4e7294fa8a1b86a40052d33295
Adela <ACprospects@outlook.com>
2026-04-29 12:14:01 +0100

    fix stripe init

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const Stripe = require("stripe");
 
 const app = express();
 app.use(cors());
